472a67f66387116cbfc079e254b91653d67d1150
persontyping <persontyping@pm.me>
2026-03-06 14:12:01 +0800

    t

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,12 +1,10 @@
-// src/app/about/page.tsx
+import Layout from '@/app/layout'
 
 export default function SettingsPage() {
+
   return (
-
-      <div className="max-w-4xl mx-auto px-6 py-12 text-[var(--color-text-primary)]">
-        <h1 className="text-3xl font-bold mb-6">SETTINGS</h1>
-
-      </div>
-
+    <Layout>
+        <h1>settings</h1>
+    </Layout>
   );
 }
