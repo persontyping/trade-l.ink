02c4dbdc3eb2dea7774fd168ca808b3cf1a30388
persontyping <persontyping@pm.me>
2026-03-06 13:31:56 +0800

    t

--- src/app/directory/page.tsx
+++ src/app/directory/page.tsx
@@ -1,9 +1,8 @@
 // src/app/about/page.tsx
-import Layout from "@/components/layout/zzz-layout";
 
 export default function DirectoryPage() {
   return (
-    <Layout>
+
       <div className="max-w-4xl mx-auto px-6 py-12">
         <h1 className="text-3xl font-bold mb-6">Directory</h1>
         <p className=" mb-4">
@@ -13,6 +12,6 @@ export default function DirectoryPage() {
           We provide an easy-to-use directory where verified women tradespeople can showcase their skills and get discovered.
         </p>
       </div>
-    </Layout>
+
   );
 }
