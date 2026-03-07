158e4cc26f04187e2dc328ecf6807bc3e99e7285
persontyping <persontyping@pm.me>
2026-03-08 03:40:29 +0800

    BD

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  /* config options here */
-};
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
 
 export default nextConfig;
